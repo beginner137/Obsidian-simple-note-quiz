9213450fac19505cde08be560128e04021e417d2
dorisxx <dorisxiang@gatech.edu>
2021-12-11 03:05:01 -0800

    code fractor

--- quiz-modal.tsx
+++ quiz-modal.tsx
@@ -6,10 +6,7 @@ import { ReactCard } from "./react-card";
 export default class QuizModal extends Modal {
     result: string;
     onSubmit: (result: string) => void;
-    view: HTMLElement;
     lines: string[];
-    currentCardNum: number;
-    currentCard: Card;
 
     constructor(app: App, onSubmit: (result: string) => void) {
         super(app);
@@ -17,14 +14,12 @@ export default class QuizModal extends Modal {
     }
 
     async onOpen() {
-        this.titleEl.setText("Let's quiz!");
-        this.titleEl.addClass("quiz__title");
         const file = this.app.workspace.getActiveFile();
-        const cards = await this.prepareCards();
-        const containerEl = this.modalEl;
+        const cards = await this.prepareCards(file);
         ReactDOM.render(
             <ReactCard cards={cards} app={this} />, this.modalEl
         );
+
         // const numOfCards = cards.length;
         // this.currentCardNum = 0;
 
@@ -76,9 +71,10 @@ export default class QuizModal extends Modal {
 
     }
 
-    async prepareCards() {
-        const file = this.app.workspace.getActiveFile();
-        const lines = await this.parseFileToLines(file);
+    async prepareCards(note: TFile) {
+        let fileText: string = await this.app.vault.read(note);
+        const lines = fileText.split("\n");
+        this.lines = lines;
         const cards = [];
         for (let i = 0; i < this.lines.length; i++) {
             let answer = "";
@@ -97,37 +93,10 @@ export default class QuizModal extends Modal {
         return cards;
     }
 
-    async parseFileToLines(note: TFile) {
-        let fileText: string = await this.app.vault.read(note);
-        this.lines = fileText.split("\n");
-        return this.lines;
-    }
-
-    async showNote(note: TFile) {
-        let fileText: string = await this.app.vault.read(note);
-        this.lines = fileText.split("\n");
-        const cards = [];
-        for (let i = 0; i < this.lines.length; i++) {
-            let answer = "";
-            if (this.lines[i].trimEnd().endsWith('?')) {
-                const question = lines[i];
-                while (i + 1 < lines.length && lines[i + 1].length !== 0) {
-                    answer += lines[i + 1] + "\n";
-                    i++;
-                }
-                cards.push([question, answer]);
-            } else {
-                continue;
-            }
-        }
-        fileText = lines.join("\n");
-        this.app.vault.modify(note, fileText);
-        return cards[0][0];
-    }
 
     onClose() {
         const { contentEl } = this;
-        ReactDOM.unmountComponentAtNode(this.modalEl);
+        ReactDOM.unmountComponentAtNode(contentEl);
         contentEl.empty();
     }
 }
--- react-card.tsx
+++ react-card.tsx
@@ -8,48 +8,73 @@ let showAnswerButton: HTMLElement;
 let checkmarkContainer: HTMLElement;
 let yesButton: HTMLElement;
 let noButton: HTMLElement;
+let partialButton: HTMLElement;
 
 export function ReactCard(props: { cards: Card[], app: any }) {
-    const [yesLines, setYesLines] = React.useState(-1);
-    const [noLines, setNoLines] = React.useState(-1);
+    const { cards, app } = props;
+
+    const [yesLines, setYesLines] = React.useState([]);
+    const [noLines, setNoLines] = React.useState([]);
     const [showAnswer, setShowAnswer] = React.useState('hide');
     const [currentCardNum, setCurrentCardNum] = React.useState(0);
-    const [container, setContainer] = React.useState(null);
-    const { cards, app } = props;
     // // add class for container
-    const containerEl = app.modalEl;
-    containerEl.addClass("quiz__container");
+    app.modalEl.addClass("quiz__container");
 
 
     React.useEffect(() => {
+        const containerEl = app.modalEl;
+        const controller = new AbortController;
         // create question area
         questionContainer = containerEl.createEl("div", { cls: "quiz__container__question__container" });
         showAnswerButton = containerEl.createEl("button", { text: "Show Answer", cls: "show-answer-btn" });
         showAnswerButton.addEventListener("click", () => {
             setShowAnswer('show');
             showAnswerButton.hide();
-        });
+        }, { signal: controller.signal });
         answerContainer = containerEl.createEl("div", { cls: "quiz__container__answer__container" });
         checkmarkContainer = containerEl.createEl("div", { cls: "quiz__container__checkmark__container" });
-        yesButton = checkmarkContainer.createEl("button", { text: "✅", cls: "yes-btn" });
-        noButton = checkmarkContainer.createEl("button", { text: "❌", cls: "no-btn" });
+        yesButton = checkmarkContainer.createEl("button", { text: "✅ Correct", cls: "yes-btn" });
+        noButton = checkmarkContainer.createEl("button", { text: "❌ Wrong ", cls: "no-btn" });
         checkmarkContainer.hide();
-        yesButton.addEventListener("click", () => {
+        const renderNextCardCleanUp = () => {
             setShowAnswer('hide');
+            setCurrentCardNum(old => old + 1);
             checkmarkContainer.hide();
-        });
+            answerContainer.empty();
+        }
+        yesButton.addEventListener("click", () => {
+            setYesLines(old => [...old, cards[currentCardNum].lineNumber]);
+            renderNextCardCleanUp();
+        }, { signal: controller.signal });
+        noButton.addEventListener("click", () => {
+            setNoLines(old => [...old, cards[currentCardNum].lineNumber]);
+            renderNextCardCleanUp();
+        }, { signal: controller.signal })
+        return () => {
+            controller.abort();
+        };
     }, [])
 
     React.useEffect(() => {
+        console.log(cards);
+        console.log(yesLines, 'yes');
+        console.log(noLines, 'no');
+        if (currentCardNum >= cards.length) {
+            app.close();
+            return;
+        }
         questionContainer.empty();
-        MarkdownRenderer.renderMarkdown(cards[0].question, questionContainer, app.app.workspace.getActiveFile(), app);
+        MarkdownRenderer.renderMarkdown(cards[currentCardNum].question, questionContainer, app.app.workspace.getActiveFile(), app);
+        if (showAnswer === 'hide') {
+            showAnswerButton.show();
+        }
 
         if (showAnswer === 'show') {
             answerContainer.empty();
-            MarkdownRenderer.renderMarkdown(cards[0].answer, answerContainer, app.app.workspace.getActiveFile(), app);
+            MarkdownRenderer.renderMarkdown(cards[currentCardNum].answer, answerContainer, app.app.workspace.getActiveFile(), app);
             checkmarkContainer.show();
         }
-    }, [showAnswer]);
+    }, [showAnswer, currentCardNum]);
 
     return (
         <>
